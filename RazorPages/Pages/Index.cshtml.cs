6ca332d5ba88516c23eb7228bb066698c5cb7372
fuyun <scfido@gmail.com>
2021-12-22 17:20:10 +0800

    添加RazorPages示例

---
+++ RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [Display(Name = "用户名")]
+    public string Name { get; set; }
+
+    [Display(Name = "密码")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost()
+    {
+
+    }
+}
