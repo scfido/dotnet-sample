eca4839714b51e15aecdbd7278c3289fc8f7bbdc
fuyun <scfido@gmail.com>
2023-12-08 15:27:24 +0800

    完成热更新示例

--- razor-pages/Host/Program.cs
+++ razor-pages/Host/Program.cs
@@ -1,20 +1,22 @@
 using System.Reflection;
 using Dotnet.Samples.RazorPages;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var mvcBuilder = builder.Services.AddRazorPages();
-mvcBuilder.PartManager.ApplicationParts.Add(new AssemblyPart(typeof(Page1Model).Assembly));
-mvcBuilder.AddRazorRuntimeCompilation(options =>
-{
-    // 获取当前进程所在的路径
-    var libPath = Path.GetFullPath("../../../../Lib", Path.GetDirectoryName(Environment.ProcessPath) ?? "") ?? throw new Exception("lib path is null");
-    Console.WriteLine("Lib path: " + libPath);
-    options.FileProviders.Add(new PhysicalFileProvider(libPath));
-});
+builder.Services
+    .AddRazorPages()
+    .AddRazorRuntimeCompilation(options =>
+    {
+        // 获取当前进程所在的路径
+        var libPath = Path.GetFullPath("../../../../Lib", Path.GetDirectoryName(Environment.ProcessPath) ?? "") ?? throw new Exception("lib path is null");
+        Console.WriteLine("Lib path: "  + libPath);
+        options.FileProviders.Add(new PhysicalFileProvider(libPath));
+    })
+    .PartManager.ApplicationParts.Add(new AssemblyPart(typeof(Page1Model).Assembly));
 
 var app = builder.Build();
 
